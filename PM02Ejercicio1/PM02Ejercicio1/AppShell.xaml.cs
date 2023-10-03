9497589a663a1bcfe27023398113e1b8fed1f930
anjimenez21 <81935849+anjimenez21@users.noreply.github.com>
2023-10-02 18:21:37 -0600

    Cambios de proyecto

--- PM02Ejercicio1/PM02Ejercicio1/AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PM02Ejercicio1;
-
-public partial class AppShell : Shell
-{
-	public AppShell()
-	{
-		InitializeComponent();
-	}
-}
--- PM02Ejercicio1/PM02Ejercicio1/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace PM02Ejercicio1;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
